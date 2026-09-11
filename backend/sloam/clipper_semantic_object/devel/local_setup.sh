bf36eb55beec9b7601b53450ef52332609626d47
sooriya <sooriya@users.noreply.github.com>
2026-09-11 16:48:34 +0800

    chore(cleanup): purge broken docker runner and prune dead clipper author artifacts

--- backend/sloam/clipper_semantic_object/devel/local_setup.sh
+++
@@ -1,1 +0,0 @@
-/home/jiuzhou/clipper_semantic_object/devel/.private/catkin_tools_prebuild/local_setup.sh
--- backend/sloam/clipper_semantic_object/devel/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig.cmake
+++
@@ -1,1 +0,0 @@
-/home/jiuzhou/clipper_semantic_object/devel/.private/catkin_tools_prebuild/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig.cmake
